edc0f2456818a1cd074aa05775eadeda7e207d4e
eudalio <tonnydallios@gmail.com>
2018-09-03 13:13:32 -0300

    Component logo

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -9,7 +9,7 @@ import Footer from '../components/template/Footer'
 export default props =>
     <div className="app">
         <Logo />
-        <Nav />
         <Main />
+        <Nav />
         <Footer />
     </div>
